8fc44e924eb0856d5b62ff84629e64d8c6aeb34a
Mohammed Alaa <medalaakanzari@duck.com>
2022-10-22 20:24:49 +0100

    Update UserResutls Component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
@@ -17,7 +19,6 @@ const UserResults = () => {
     });
 
     const data = await response.json();
-
     setUsers(data);
     setLoading(false);
   };
@@ -28,12 +29,12 @@ const UserResults = () => {
       lg:grid-col-3 md:grid-cols-2"
       >
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   } else {
-    <h3>Loading ....</h3>;
+    return <Spinner />;
   }
 };
 
